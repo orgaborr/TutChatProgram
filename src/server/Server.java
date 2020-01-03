1f6d86904a182fc67b98cc5bf35f79c9def36581
Gabor <orbangabeszb@gmail.com>
2020-01-03 22:05:43 +0100

    server.Server class methods sketched up

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,4 +2,23 @@ package server;
 
 public class Server {
 
+	public static void start(int port) {
+		
+	}
+	
+	private static void broadcast() {
+		
+	}
+	
+	private static void send() {
+		
+	}
+	
+	private static void listen() {
+		
+	}
+	
+	public static void stop() {
+		
+	}
 }
