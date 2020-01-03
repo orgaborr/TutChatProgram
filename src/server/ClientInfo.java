dfdbb1d077a72449624835cad30507633f08d972
Gabor <orbangabeszb@gmail.com>
2020-01-03 22:18:13 +0100

    server.ClientInfo constructor and getters added

--- src/server/ClientInfo.java
+++ src/server/ClientInfo.java
@@ -8,4 +8,28 @@ public class ClientInfo {
 	private int port;
 	private String name;
 	private int id;
+	
+	public ClientInfo(String name, int id, InetAddress address, int port) {
+		this.name = name;
+		this.id = id;
+		this.address = address;
+		this.port = port;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public InetAddress getAddress() {
+		return address;
+	}
+
+	public int getPort() {
+		return port;
+	}
+	
 }
